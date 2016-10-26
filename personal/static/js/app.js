04cfe353d0c4bde0648ed93a049c80fbe13a0e62
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-10-26 15:13:03 -0600

    Initial commit

---
+++ personal/static/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("djangoApp", ['ngRoute', 'ngMaterial'])
+app.config(function($interpolateProvider, $routeProvider, $locationProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+    // $routeProvider
+    // .when('/', {
+    //     templateUrl: 'home.html'
+    // })
+    // .when('/contact/', {
+    //     templateUrl: 'basic.html'
+    // })
+    $locationProvider.html5Mode(true);
+    console.log("Here")
+});
---
+++ personal/static/js/controller.js
@@ -0,0 +1,3 @@
+app.controller("mainAppCtrl", function($scope) {
+    
+});
