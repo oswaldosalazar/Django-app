a2ab6768637536eb1d37afac566736cdacc360cc
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-05 22:21:24 -0600

    Added portfolio tiles

--- personal/static/js/controller.js
+++ personal/static/js/controller.js
@@ -1,4 +1,20 @@
 app.controller("mainAppCtrl", function($scope) {
     $scope.view = {};
-    $scope.view.title = "Portfolio";
+    $scope.view.portfolio = [
+        {
+            "title": "GigList",
+            "description": "Live music search app",
+            "appUrl": "https://giglist-q1-f4495.firebaseapp.com"
+        },
+        {
+            "title": "Captain's Log",
+            "description": "Blog with posts and comments",
+            "appUrl": "https://captainslog-1.herokuapp.com/"
+        },
+        {
+            "title": "GnarCar",
+            "description": "Carpool app for skiers and snowboarders",
+            "appUrl": "https://gnarcar.herokuapp.com/"
+        }
+    ]
 });
